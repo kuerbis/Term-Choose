67e8941d41cd541cf19f59613e5ee69b4d6fbfce
kuerbis <cuer2s@gmail.com>
2021-09-17 17:23:17 +0200

    Undo the previous "bugfix". Bugfix in current_layout

--- lib/Term/Choose/Linux.pm
+++ lib/Term/Choose/Linux.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use 5.008003;
 
-our $VERSION = '1.738';
+our $VERSION = '1.739';
 
 use Term::Choose::Constants qw( :all );
 use Term::Choose::Screen    qw( hide_cursor show_cursor normal );
